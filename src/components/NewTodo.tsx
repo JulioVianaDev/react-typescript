d96382f3a0ada667a60ec629467c12fafe9223d1
juliovianadev <juliovianadev@gmail.com>
2023-04-10 19:34:10 -0300

    validação

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const NewTodo = ()=>{
+  const todoTextInput = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent)=>{
     event.preventDefault();
-    
+    const enteredText= todoTextInput.current!.value;
+
+    if(enteredText.trim().length === 0){
+      return
+    }
   }
+
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text" >todo text</label>
-      <input type="text"  id='text' />
+      <input type="text"  id='text' ref={todoTextInput} />
       <button>Enviar</button>
     </form>
   )

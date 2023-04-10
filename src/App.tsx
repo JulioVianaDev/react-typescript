0a44f31d9e7d980e63811dc70181a664900a95eb
juliovianadev <juliovianadev@gmail.com>
2023-04-10 19:16:10 -0300

    usando o model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Todos from './components/Todos';
-
+import Todo from './models/todo';
 import './App.css';
 
 function App() {
+  const todos =[
+    new Todo('aprendendo react'),
+    new Todo('aprendendo com typescript'),
+  ];
   return (
     <div className="App">
       <Todos items={['learn react','i love react']}/>

b763bd5c51926bc9d33079214347e4e9e3b8fe6e
juliovianadev <juliovianadev@gmail.com>
2023-04-10 19:39:58 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 import './App.css';
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos =[
     new Todo('aprendendo react'),
     new Todo('aprendendo com typescript'), 
   ];
+
+  const addTodo=(texto: string)=>{
+    let newTodo = new Todo(texto);
+    todos.push(newTodo);
+  }
   return (
     <div className="App">
+      <NewTodo onAddTodo={addTodo}/>
       <Todos items={todos}/>
     </div>
   );
